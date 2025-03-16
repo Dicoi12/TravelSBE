a9d7153365eda370c4240ec8f550aa17baf30255
Dicoi12 <dicoidarius1202@gmail.com>
2025-03-16 20:26:01 +0200

    new migration

--- Entity/ObjectiveImage.cs
+++ Entity/ObjectiveImage.cs
@@ -1,6 +1,7 @@
 ﻿ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
+using TravelsBE.Entity;
 using TravelSBE.Entity.Helper;
 
 namespace TravelSBE.Entity
@@ -20,5 +21,8 @@ namespace TravelSBE.Entity
         [ForeignKey("IdEvent")]
         public int? IdEvent { get; set; }
         public Event? Event { get; set; }
+        [ForeignKey("IdExperienta")]
+        public int? IdExperienta { get; set; }
+        public Experience? Experience { get; set; }
     }
 }
--- Migrations/20250316164102_experienceImage.Designer.cs
+++ Migrations/20250316164102_experienceImage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TravelSBE.Data;
@@ -11,9 +12,11 @@ using TravelSBE.Data;
 namespace TravelSBE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250316164102_experienceImage")]
+    partial class experienceImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
